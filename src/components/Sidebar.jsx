bf5a6ec4b3b191e085f8507f338725b359344c50
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-10-04 22:27:37 +0700

    mengubah logo pada sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import logoWecare from '../assets/logoWecare.png';
 
 const Sidebar = () => {
   // State untuk toggle sidebar
@@ -10,22 +11,23 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`fixed top-0 left-0 h-full ${isOpen ? 'w-64' : 'w-20'} bg-pink-300 transition-width duration-300 shadow-lg`}>
+    <div className={`fixed top-0 left-0 h-full ${isOpen ? 'w-64' : 'w-20'} bg-pink-300 transition-all duration-300 shadow-lg rounded-r-xl`}>
       <div className="flex items-center justify-center py-4">
-        <img src="logo.png" alt="WeCare Logo" className="w-10 h-10" />
+        <img src={logoWecare} className="w-10 h-10" />
+
         {isOpen && <h1 className="ml-4 text-2xl font-bold text-white">WeCare</h1>}
       </div>
       <ul className="mt-10 space-y-4">
         <li className="flex items-center space-x-4 p-2 hover:bg-pink-400 cursor-pointer">
-          <i className="text-white text-2xl fas fa-home"></i>
+          <i className="text-red-600 text-2xl fas fa-home"></i>
           {isOpen && <span className="text-white">Home</span>}
         </li>
         <li className="flex items-center space-x-4 p-2 hover:bg-pink-400 cursor-pointer">
-          <i className="text-white text-2xl fas fa-clipboard"></i>
+          <i className="text-red-600 text-2xl fas fa-clipboard"></i>
           {isOpen && <span className="text-white">Test</span>}
         </li>
         <li className="flex items-center space-x-4 p-2 hover:bg-pink-400 cursor-pointer">
-          <i className="text-white text-2xl fas fa-users"></i>
+          <i className="text-red-600 text-2xl fas fa-users"></i>
           {isOpen && <span className="text-white">Komunitas</span>}
         </li>
       </ul>
